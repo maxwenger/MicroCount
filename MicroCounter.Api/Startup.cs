158a7ac2ce20d0f071a1cfe5d779ef513a9bbebb
maxwenger <maxwenger@users.noreply.github.com>
2017-10-21 05:54:13 -0700

    got api docker stuff setup and working, havent intigated with redis oranything yet

---
+++ MicroCounter.Api/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Nancy.Owin;
+
+namespace MicroCounter.Api 
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseOwin(x => x.UseNancy());
+        }
+    }
+}
+
